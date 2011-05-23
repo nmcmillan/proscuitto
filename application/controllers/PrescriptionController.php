8e8ed320aef8b265c2435f973abbacca31997660
bkurinsky <blake.kurinsky@gmail.com>
2011-05-23 18:54:50 -0500

    Created initial databases for web application.

---
+++ application/controllers/PrescriptionController.php
@@ -0,0 +1,39 @@
+<?php
+
+class PrescriptionController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function addAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ application/models/DbTable/Prescriptions.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Prescriptions extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'prescriptions';
+
+
+}
+
