cd07d47127d02607290ef632c0fcaa5bed6840b4
bkurinsky <blake.kurinsky@gmail.com>
2011-05-23 21:08:29 -0500

    Started adding some information to the Patient 'add' form.

--- application/forms/Patient.php
+++ application/forms/Patient.php
@@ -6,6 +6,11 @@ class Application_Form_Patient extends Zend_Form
     public function init()
     {
         /* Form Elements & Other Definitions Here ... */
+    	$this->setName('patient');
+    	
+    	
+    	
+    	
     }
 
 
